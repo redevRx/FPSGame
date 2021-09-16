a68e1736ea7bc86461c00f34b94d3c9d2f4cba57
redevRx <redev.rx@gmail.com>
2021-09-16 11:45:54 +0700

    gen

---
+++ Assets/kleeMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kleeMove : MonoBehaviour
+{
+
+    private int movementspeed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
+        }
+      
+    }
+}
