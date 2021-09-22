f25e0fedef40ed852a20e31637523160b885fc45
redevRx <redev.rx@gmail.com>
2021-09-23 00:32:51 +0700

    character controller and fig bug falling map

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
-    public Transform playerBody;
-
-    public float clampAngle = 80.0f;
+    public float mouseSensitivity = 2f;
+   // public Transform playerBody;
 
     private float rotY = 0.0f; // rotation around the up/y axis
     private float rotX = 0.0f; // rotation around the right/x axis
+
+
     // Start is called before the first frame update
     void Start()
     {
         LockMosue();
-        SetUpRatate();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //
         MouseMove();
     }
 
+
+ 
     //code on video not working
     //Search Mouse Looking
     //code from https://answers.unity.com/questions/29741/mouse-look-script.html
@@ -38,20 +37,17 @@ public class MouseLook : MonoBehaviour
         rotY += mouseX * mouseSensitivity * Time.deltaTime;
         rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        rotX = Mathf.Clamp(rotX, -80f, 80f);
 
         Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
         transform.rotation = localRotation;
-        playerBody.Rotate(Vector3.up * mouseX);
     }
+
+  
 
     private void LockMosue()
     {
         Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private void SetUpRatate()
-    {
         Vector3 rot = transform.localRotation.eulerAngles;
         rotY = rot.y;
         rotX = rot.x;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float jumpHeight;
+    [SerializeField] private float moveSpeed = 4.5f;
+    [SerializeField] private float jumpHeight = 1.5f;
+    
 
-   
     private Vector3 velocity;
+    private Vector3 movement;
 
     [SerializeField] private float gravity;
     [SerializeField] private bool isGraounded;
@@ -22,15 +21,16 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //enable for youtube style
-        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerMove();
+
+       PlayerMove();
     }
+
+
 
     //code from video style
     //result not working
@@ -44,61 +44,23 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2f;
         }
 
-        float moveZ = Input.GetAxis("Vertical");
-        float moveX = Input.GetAxis("Horizontal");
 
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        characterController.Move(move * moveSpeed * Time.deltaTime);
+         movement = new Vector3(Mathf.RoundToInt(Input.GetAxis("Horizontal")), 0, Mathf.RoundToInt(Input.GetAxis("Vertical"))).normalized;
+         characterController.Move(movement * Time.deltaTime * moveSpeed);
 
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
-    }
-
-    //code from youtube player movement
-    private Vector3 moveDirection;
-
-    private void Move()
-    {
-        isGraounded = characterController.isGrounded;
-
-        if (isGraounded && velocity.y < 0)
-        {
-            gravity = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * moveSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-        {
-            //walk
-            Walk();
-        }
-        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-        {
-            //run
-            Run();
-        }
-        else if (moveDirection == Vector3.zero)
-        {
-            //idel
-            Idel();
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && isGraounded)
         {
             Jump();
         }
 
-        //velocity.y += gravity * Time.deltaTime;
-        //characterController.Move(velocity * Time.deltaTime);
+      
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity);
     }
+
+
+
 
     private void Idel()
     {
@@ -107,12 +69,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void Walk()
     {
-        moveSpeed = walkSpeed;
+        moveSpeed = 5f;
     }
 
     private void Run()
     {
-        moveSpeed = runSpeed;
+        moveSpeed = 10f;
     }
 
     private void Jump()
